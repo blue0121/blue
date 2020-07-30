6e95d08ca5ce8de0d5286014e73a6036f7db6476
Jin Zheng <jin.zheng@ericsson.com>
2020-07-30 17:46:58 +0800

    add reflect package

--- blue-core/src/main/java/blue/core/reflect/BeanField.java
+++ blue-core/src/main/java/blue/core/reflect/BeanField.java
@@ -9,21 +9,57 @@ import java.lang.reflect.Method;
  */
 public interface BeanField
 {
-
+	/**
+	 * field name
+	 * @return
+	 */
 	String getFieldName();
 
+	/**
+	 * column name of database table
+	 * @return
+	 */
 	String getColumnName();
 
+	/**
+	 * target object
+	 * @return
+	 */
 	Object getTarget();
 
+	/**
+	 * 1. invoke getter method<br/>
+	 * 2. field.setAccessible(true) & field.get(target)
+	 * @return
+	 */
 	Object getFieldValue();
 
+	/**
+	 * 1. invoke setter method<br/>
+	 * 2. field.setAccessible(true) & field.set(target, value)
+	 * @param value
+	 */
 	void setFieldValue(Object value);
 
+	/**
+	 * getter method
+	 * @return
+	 */
 	Method getGetterMethod();
 
+	/**
+	 * setter method
+	 * @return
+	 */
 	Method getSetterMethod();
 
+	/**
+	 * 1. setter method annotation
+	 * 2. field annotation
+	 * @param annotationClass
+	 * @param <T>
+	 * @return
+	 */
 	<T extends Annotation> T getAnnotation(Class<T> annotationClass);
 
 }
--- blue-core/src/main/java/blue/core/reflect/JavaBean.java
+++ blue-core/src/main/java/blue/core/reflect/JavaBean.java
@@ -11,20 +11,48 @@ import java.util.Map;
  */
 public interface JavaBean
 {
-
+	/**
+	 * create JavaBean object
+	 * @param target
+	 * @param targetClass
+	 * @return
+	 */
 	static JavaBean parse(Object target, Class<?> targetClass)
 	{
 		return new DefaultJavaBean(target, targetClass);
 	}
 
+	/**
+	 * target real class
+	 * @return
+	 */
 	Class<?> getTargetClass();
 
+	/**
+	 * target object
+	 * @return
+	 */
 	Object getTarget();
 
+	/**
+	 * class annotation
+	 * @param annotationClass
+	 * @param <T>
+	 * @return
+	 */
 	<T extends Annotation> T getAnnotation(Class<T> annotationClass);
 
+	/**
+	 * get all BeanField
+	 * @return
+	 */
 	Map<String, BeanField> getAllFields();
 
+	/**
+	 * get BeanField by field name
+	 * @param fieldName
+	 * @return
+	 */
 	BeanField getField(String fieldName);
 
 }
