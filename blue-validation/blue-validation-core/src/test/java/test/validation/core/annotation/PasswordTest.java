6cbc485397f37e429a099ea708ff424e2b6f75e1
zhengjin <89609719@qq.com>
2021-06-22 16:46:46 +0800

    remove DigestUtil tool

--- blue-validation/blue-validation-core/src/test/java/test/validation/core/annotation/PasswordTest.java
+++ blue-validation/blue-validation-core/src/test/java/test/validation/core/annotation/PasswordTest.java
@@ -1,6 +1,8 @@
 package test.validation.core.annotation;
 
-import blue.base.core.security.DigestUtil;
+import blue.base.core.security.Digest;
+import blue.base.core.security.DigestType;
+import blue.base.core.security.SecurityFactory;
 import blue.validation.core.ValidationUtil;
 import blue.validation.core.group.UpdateGroup;
 import jakarta.validation.ValidationException;
@@ -13,14 +15,17 @@ import test.validation.core.model.User;
  * @since 1.0 2020-06-23
  */
 public class PasswordTest {
+	private Digest md5Digest = SecurityFactory.createDigest(DigestType.MD5);
+	private Digest sha1Digest = SecurityFactory.createDigest(DigestType.SHA1);
+
 	public PasswordTest() {
 	}
 
 	@Test
 	public void test1() {
 		User user = new User();
-		user.setPassword(DigestUtil.md5Hex("password"));
-		user.setRePassword(DigestUtil.sha1Hex("password"));
+		user.setPassword(md5Digest.digestToHex("password"));
+		user.setRePassword(sha1Digest.digestToHex("password"));
 		ValidationUtil.valid(user, UpdateGroup.class);
 	}
 
@@ -36,7 +41,7 @@ public class PasswordTest {
 	public void test3() {
 		User user = new User();
 		user.setPassword("012345678901234567890123456789tf");
-		user.setRePassword(DigestUtil.sha1Hex("password"));
+		user.setRePassword(sha1Digest.digestToHex("password"));
 		Assertions.assertThrows(ValidationException.class, () -> ValidationUtil.valid(user, UpdateGroup.class));
 	}
 
